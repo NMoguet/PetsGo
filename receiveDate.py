8dd888b4f3ae576957a9532075986dab1e37e65f
NMoguet <93800005+NMoguet@users.noreply.github.com>
2023-02-28 12:05:26 +0700

    Update receiveDate.py
    Added threading to code to allow it to continue to run

--- receiveDate.py
+++ receiveDate.py
@@ -1,4 +1,5 @@
 import pika
+import threading
 
 # Connect to RabbitMQ
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
@@ -11,9 +12,14 @@ channel.queue_declare(queue='date_response_queue')
 def response_callback(ch, method, properties, body):
     print(f"Received date: {body.decode()}")
 
-# Start consuming messages on the response queue
-channel.basic_consume(queue='date_response_queue', on_message_callback=response_callback, auto_ack=True)
+# Start a new thread for consuming messages
+def consume_messages():
+    # Start consuming messages on the response queue
+    channel.basic_consume(queue='date_response_queue', on_message_callback=response_callback, auto_ack=True)
 
-# Wait for messages
-print("Waiting for date response...")
-channel.start_consuming()
+    # Wait for messages
+    print("Waiting for date response...")
+    channel.start_consuming()
+
+message_thread = threading.Thread(target=consume_messages)
+message_thread.start()
